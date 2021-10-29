449209b2591b5ee8f342ff4d090e9554d83cdcdb
Mikkel Aas <aa.mikkel2902@gmail.com>
2021-10-29 15:47:37 +0200

    Added a button that takes you to the users images.

--- components/userDetail/userDetail.jsx
+++ components/userDetail/userDetail.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
-import { Divider } from '@material-ui/core';
+import { 
+    Divider,
+    Button,
+    Box
+} from '@material-ui/core';
 import './userDetail.css';
+import { Link } from "react-router-dom";
+
 
 /**
  * Define UserDetail, a React componment of CS142 project #5
@@ -28,11 +34,16 @@ class UserDetail extends React.Component {
             <div key={this.props.match.params.userId}>
                 <h1>{this.state.user.first_name}  {this.state.user.last_name}</h1>
                 <p>
-                    <b>About me:</b> {this.state.user.description} &nbsp;
+                    <b>About me:</b> {this.state.user.description} <br />
                     <b>Location:</b> {this.state.user.location} &nbsp;
                     <b>Ocupation:</b> {this.state.user.occupation}
                 </p>
                 <Divider />
+                <Box mt="10px">
+                    <Link>
+                        <Button component={Link} to={`/photos/${this.state.user._id}`} variant="contained">My images</Button>
+                    </Link>
+                </Box>
             </div>
         );
     }
