79c66c143af9f3bea41b34ba7b54b76c9ac366b9
Ruben Christoffer <rubenc@protonmail.com>
2021-11-04 21:58:02 +0100

    UserPhotos: Improved layout

--- components/userPhotos/userPhotos.jsx
+++ components/userPhotos/userPhotos.jsx
@@ -12,9 +12,13 @@ import {
     CardActions,
     IconButton,
     CardContent,
-    Button
+    Button,
+    Container
 } from '@material-ui/core';
 import Image from 'material-ui-image';
+
+import { withStyles, createStyles } from '@material-ui/core';
+
 
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import ExpandLessIcon from '@material-ui/icons/ExpandLess';
@@ -33,60 +37,50 @@ class UserPhotos extends React.Component {
         };
     }
 
-    toggleCollapse (index) {
+    toggleCollapse(index) {
         this.setState({ [index]: !this.state[index] });
     }
 
+
+
     render() {
         return (
-            <Grid
-                container
-                spacing={5}
-                direction="column"
-                alignItems="center"
-                style={{ minHeight: '100vh' }}>
+            <div className="userPhotosContainer">
                 {
                     window.cs142models.photoOfUserModel(this.props.match.params.userId).map((photo, index) =>
-                        <Grid
-                            item
-                            sm={12}
-                            key={photo._id}
-                            className="userPhotosGridItem">
-                            <Card>
-                                <CardHeader
-                                    title={photo.file_name}
-                                    subheader={photo.date_time}
-                                />
-                                <CardMedia
-                                    component="img"
-                                    height="194"
-                                    image={`/images/${photo.file_name}`}
-                                    alt={"Could not display photo!"}
-                                />
-                                <CardActions>
-                                    <IconButton onClick={(e) => this.toggleCollapse(index)}>
-                                        <Typography>Comments</Typography>
-                                        {this.state[index] ? < ExpandLessIcon/> : <ExpandMoreIcon />}
-                                    </IconButton>
-                                </CardActions>
-                                <Collapse in={this.state[index]} timeout="auto" unmountOnExit>
-                                    <CardContent>
-                                        <p>COMMENTS</p>
-                                    </CardContent>
-                                </Collapse>
+                        <Card key={photo._id} variant="outlined">
+                            <CardHeader
+                                title={photo.file_name}
+                                subheader={photo.date_time}
+                            />
+                            <CardMedia
+                                component="img"
+                                image={`/images/${photo.file_name}`}
+                                alt={"Could not display photo!"}
+                            />
+                            <CardActions>
+                                <IconButton onClick={(e) => this.toggleCollapse(index)}>
+                                    <Typography>Comments</Typography>
+                                    {this.state[index] ? < ExpandLessIcon /> : <ExpandMoreIcon />}
+                                </IconButton>
+                            </CardActions>
+                            <Collapse in={this.state[index]} timeout="auto" unmountOnExit>
+                                <CardContent>
+                                    <p>COMMENTS</p>
+                                </CardContent>
+                            </Collapse>
 
-                                {/*<img src={}/>
+                            {/*<img src={}/>
                             <p>{photo.date_time}</p>
                             {
                                 photo.comments.map(comment =>
                                     <Comment text={comment.comment} />
                                 )
                             }*/}
-                            </Card>
-                        </Grid>
+                        </Card>
                     )
                 }
-            </Grid>
+            </div>
         );
     }
 }
