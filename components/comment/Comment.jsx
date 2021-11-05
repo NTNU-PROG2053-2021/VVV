224c5add505674427e40d5c4b3f08dcfa47668ff
Ruben Christoffer <rubenc@protonmail.com>
2021-11-05 15:52:54 +0100

    Comment: Converted to functional component and removed unnecessary imports

--- components/comment/Comment.jsx
+++ components/comment/Comment.jsx
@@ -1,36 +1,27 @@
 import React from 'react';
 import {
-    AppBar, Card, CardContent, CardHeader, Toolbar, Typography
+    Card, CardContent, Typography
 } from '@material-ui/core';
 import './Comment.css';
 import { Link } from 'react-router-dom';
 
-class Comment extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
-        return (
-            <Card variant="outlined">
-                <CardContent>
-                    <Typography>
-                        {this.props.text}
-                    </Typography>
-                    <br />
+export const Comment = ({ text, date_time, user }) => {
+    return (
+        <Card variant="outlined">
+            <CardContent>
+                <Typography>
+                    {text}
+                </Typography>
+                <br />
+                <Typography variant="body2">
+                    {date_time}
+                </Typography>
+                <Link to={`/users/${user._id}`}>
                     <Typography variant="body2">
-                        {this.props.date_time}
+                        {user.first_name} {user.last_name}
                     </Typography>
-                    <Link to={`/users/${this.props.user._id}`}>
-                        <Typography variant="body2">
-                            {this.props.user.first_name} {this.props.user.last_name}
-                        </Typography>
-                    </Link>
-                </CardContent>
-            </Card>
-
-        );
-    }
+                </Link>
+            </CardContent>
+        </Card>
+    )
 }
-
-export default Comment;
