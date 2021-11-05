20cd2a240f4d97d68900b957b485761cf513298f
Ruben Christoffer <rubenc@protonmail.com>
2021-11-05 15:38:36 +0100

    UserPhotos: Completed Comment component

--- components/comment/Comment.jsx
+++ components/comment/Comment.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import {
-    AppBar, Toolbar, Typography
+    AppBar, Card, CardContent, CardHeader, Toolbar, Typography
 } from '@material-ui/core';
 import './Comment.css';
+import { Link } from 'react-router-dom';
 
 class Comment extends React.Component {
     constructor(props) {
@@ -11,7 +12,23 @@ class Comment extends React.Component {
 
     render() {
         return (
-           <p>{this.props.text}</p> 
+            <Card variant="outlined">
+                <CardContent>
+                    <Typography>
+                        {this.props.text}
+                    </Typography>
+                    <br />
+                    <Typography variant="body2">
+                        {this.props.date_time}
+                    </Typography>
+                    <Link to={`/users/${this.props.user._id}`}>
+                        <Typography variant="body2">
+                            {this.props.user.first_name} {this.props.user.last_name}
+                        </Typography>
+                    </Link>
+                </CardContent>
+            </Card>
+
         );
     }
 }
