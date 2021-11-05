a39e05a37709480c1d2b6ca4980448bc9d033b47
Jakob <jakkar@mailbox.org>
2021-11-05 15:31:37 +0100

    TopBar now displays app context

--- components/topBar/TopBar.jsx
+++ components/topBar/TopBar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {
-    AppBar, Toolbar, Typography
+    AppBar, Toolbar, Typography, Box
 } from '@material-ui/core';
 import './TopBar.css';
 
@@ -10,14 +10,43 @@ import './TopBar.css';
 class TopBar extends React.Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            user: "",
+            display: "",
+            currentPage: "",
+        }
     }
+
+    componentDidUpdate(prevProps) {
+        if (this.props !== prevProps) {
+            this.setState({
+                user: window.cs142models.userModel(this.props.match.params.userId),
+                currentPage: this.props.match.path
+            }, this.updateDisplay)
+        }
+    }
+
+    updateDisplay() {
+        if (this.state.currentPage.startsWith("/user")) {
+            this.setState({ display: this.state.user.first_name + " " + this.state.user.last_name })
+        } else if (this.state.currentPage.startsWith("/photos")) {
+            this.setState({ display: "Photos of " + this.state.user.first_name + " " + this.state.user.last_name })
+        }
+    }
+
 
     render() {
         return (
             <AppBar className="cs142-topbar-appBar" position="absolute">
                 <Toolbar>
+                    <Box display='flex' flexGrow={1}>
+                        <Typography variant="h5" color="inherit">
+                            Verdens Vide Vev
+                        </Typography>
+                    </Box>
                     <Typography variant="h5" color="inherit">
-                        Verdens Vide Vev
+                        {this.state.display}
                     </Typography>
                 </Toolbar>
             </AppBar>
